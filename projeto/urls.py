4dadfb586b2d448f10475e8b5b1eaa25120a870e
daniel-estevao <daniel.pardinho@gpr.com.br>
2023-10-18 09:50:35 -0300

    aninhando URLs

--- projeto/urls.py
+++ projeto/urls.py
@@ -18,4 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("recipes.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("recipes.urls")),
+    path("recipes/", include("recipes.urls")),
+]
